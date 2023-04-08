d770f29f818e983268e6d4911e6231e14820ae1d
Tim McManemy <tim.mcmanemy@gmail.com>
2023-04-07 23:30:33 -0500

    Returns the temperature for dynamic locations

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,14 @@
 const axios = require('axios/dist/node/axios.cjs');
 
 export function cli(args) {
-	axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=St.Louis&appid=${args[2]}`).
+	axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${args.city}&appid=${args.apiKey}`).
 		then((response) => {
 			let lat = response.data[0].lat;
 			let lon = response.data[0].lon;
 
-			axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${args[2]}&units=imperial`).
+			axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${args.apiKey}&units=imperial`).
 				then((response) => {
-					console.log(response.data.main.temp);
+					console.log(`The current temperature in ${args.city} is ${response.data.main.temp}`);
 				});
 		})
 }
