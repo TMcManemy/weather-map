5fb58a33590446d0bbe3e4fdb5a7128e8df7cbcb
Tim McManemy <tim.mcmanemy@gmail.com>
2023-04-08 23:14:18 -0500

    Allows searching by city name, zipcode, and coordinates

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,42 @@
 const axios = require('axios/dist/node/axios.cjs');
 
-export function cli(args) {
-	axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${args.city}&appid=${args.apiKey}`).
-		then((response) => {
-			let lat = response.data[0].lat;
-			let lon = response.data[0].lon;
+export async function cli(args) {
+	validateArguments(args);
 
-			axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${args.apiKey}&units=imperial`).
-				then((response) => {
-					console.log(`The current temperature in ${args.city} is ${response.data.main.temp}`);
-				});
-		})
+	args = await setArguments(args);
+
+	let weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${args.coordinates[0]}&lon=${args.coordinates[1]}&appid=${args.apiKey}&units=imperial`);
+
+	console.log(`The current temperature in ${args.city} is ${weather.data.main.temp}`);
+}
+
+function validateArguments(args){
+	if(!(args.city || args.coordinates || args.zipcode))
+		throw new Error('city, coordinates, or zipcode is required');
+
+	if(args.coordinates && args.coordinates.length !== 2)
+		throw new Error('latitude and longitude coordinates are expected');
+}
+
+async function setArguments(args) {
+	if (args.city) {
+		let geo = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${args.city}&appid=${args.apiKey}`);
+		args.coordinates = [];
+		args.coordinates[0] = geo.data[0].lat;
+		args.coordinates[1] = geo.data[0].lon;
+		args.city = geo.data[0].name;
+	}
+	else if (args.zipcode) {
+		let geo = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${args.zipcode}&appid=${args.apiKey}`);
+		args.coordinates = [];
+		args.coordinates[0] = geo.data.lat;
+		args.coordinates[1] = geo.data.lon;
+		args.city = geo.data.name;
+	}
+	else {
+		let geo = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${args.coordinates[0]}&lon=${args.coordinates[1]}&appid=${args.apiKey}`);
+		args.city = geo.data[0].name;
+	}
+
+	return args;
 }
