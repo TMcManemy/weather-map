1f65a44dbb7026bb373740039dc2660d61d46e33
Tim McManemy <tim.mcmanemy@gmail.com>
2023-04-09 23:35:49 -0500

    Adds a few tests

---
+++ test/cli.test.js
@@ -0,0 +1,16 @@
+require = require('esm')(module);
+const cli = require('../src/cli.js')
+
+describe('input validation', () => {
+	test('throws if args not provided', () => {
+		expect(async () => { await cli.cli({}); }).rejects.toThrow(Error);
+	});
+
+	test('throws if city, coordinates, or zipcode is not provided', () => {
+		expect(async () => { await cli.cli({apiKey:"key"}); }).rejects.toThrow(Error);
+	});
+
+	test('throws if coordiantes are provided without latitude and longitude', () => {
+		expect(async () => { await cli.cli({coordinates: []}); }).rejects.toThrow(Error);
+	});
+});
